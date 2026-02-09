b0db8c6fcac2862a29ac3482da6768425c43b31a
sanjalee2003 <sanjaleedassanayake56@gmail.com>
2026-02-09 18:33:54 +0530

    Initial commit

---
+++ Backend/demo 3/src/test/java/syncX/InterLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package syncX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
