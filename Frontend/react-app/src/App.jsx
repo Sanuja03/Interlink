3f5e41cecf844da9592f0074698179953d21fbe1
Sanuja Alphonsus <nimsith@gmail.com>
2026-04-11 12:10:36 +0530

    feat: add new routing for dashboard pages

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -3,6 +3,13 @@ import AllActivitiesPage from "./pages/AllActivities";
 import DashboardLayout from "./components/Layout/Dashboardlayout";
 import SuperAdminDashboard from "./pages/SuperAdminDashboard";
 import SuperAdminCompanies from "./pages/SuperAdminCompanies";
+import SuperAdminInterviews from "./pages/SuperAdminInterviews";
+import SuperAdminProfile from "./pages/SuperAdminProfile";
+import SystemSettings from "./pages/SystemSettings";
+import SuperAdminJobs from "./pages/SuperAdminJobs";
+import SuperAdminJobDetails from "./pages/SuperAdminJobDetails";
+import SuperAdminUsers from "./pages/SuperAdminUsers";
+import ChatBot from "./components/RagChatbot/ChatBot";
 
 function App() {
   return (
@@ -11,6 +18,13 @@ function App() {
         <Route path="/SuperAdmin/dashboard" element={<DashboardLayout><SuperAdminDashboard /></DashboardLayout>} />
         <Route path="/SuperAdmin/AllActivities" element={<DashboardLayout><AllActivitiesPage /></DashboardLayout>} />
         <Route path="/SuperAdmin/Companies" element={<DashboardLayout><SuperAdminCompanies /></DashboardLayout>} />
+        <Route path="/SuperAdmin/Interviews" element={<DashboardLayout><SuperAdminInterviews /></DashboardLayout>} />
+        <Route path="/SuperAdmin/Profile" element={<DashboardLayout><SuperAdminProfile /></DashboardLayout>} />
+        <Route path="/SuperAdmin/SystemSettings" element={<DashboardLayout><SystemSettings /></DashboardLayout>} />
+        <Route path="/SuperAdmin/Jobs" element={<DashboardLayout><SuperAdminJobs /></DashboardLayout>} />
+        <Route path="/SuperAdmin/Jobs/:id" element={<DashboardLayout><SuperAdminJobDetails /></DashboardLayout>} />
+        <Route path="/SuperAdmin/Users" element={<DashboardLayout><SuperAdminUsers /></DashboardLayout>} />
+        <Route path="/SuperAdmin/ChatBot" element={<DashboardLayout><ChatBot /></DashboardLayout>} />//test for now
       </Routes>
     </BrowserRouter>
   );
